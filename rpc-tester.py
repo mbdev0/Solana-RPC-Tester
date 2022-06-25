28669b3e21943b265ef003c63dcf5f15c588403a
mbdev0 <mustafababar7@gmail.com>
2022-06-25 17:14:40 +0100

    Added base functionality + error handling

---
+++ rpc-tester.py
@@ -0,0 +1,44 @@
+import requests
+import time
+
+def test_rpc(url):
+    payload = {"jsonrpc":"2.0","id":"7","method":"getRecentBlockhash","params":[]}
+    headers={'Content-Type': 'application/json'}
+    total = 0
+
+    try:
+        for i in range(10):
+            start = time.perf_counter()
+            x=requests.post(url=url,json=payload,headers=headers)
+            final = time.perf_counter() - start
+
+            if x.status_code != 200:
+                if x.status_code == 405:
+                    return 'Make sure you are using a valid rpc'
+                elif x.status_code == 403:
+                    return 'Make sure you have your IP whitelisted'
+                else:
+                    return x.status_code
+
+            print(f"Request {i+1} "+ "completed in {0:.6f}ms".format(final))
+            total += final
+
+        return 'Average: {0:.6f}ms'.format(total/10) 
+    except requests.exceptions.MissingSchema:
+        return 'Invalid URL'
+    except requests.exceptions.ConnectionError:
+        return 'Check URL again'
+finished = False
+while not finished:
+    rpc = input('Enter a RPC: ')
+    print(test_rpc(rpc))
+
+    try_again = input('Try another RPC? (y/n): ')
+    
+    if try_again.lower() == 'n':
+        print('Exiting...')
+        finished = True
+    elif try_again.lower() == 'y':
+        continue
+    else:
+        print("Enter a valid string")
